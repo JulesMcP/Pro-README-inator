8b3135ab64e816549ed1f2839ddbbd5e30061649
JulesMcP <rogue427@yahoo.com>
2022-02-20 11:49:28 -0600

    inserted user input array

--- index.js
+++ index.js
@@ -1,16 +1,130 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-    console.log(inquirer);
-// const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title', 
+            message: 'What is your project title? (Required)', 
+            validate: nameInput => {
+                if (nameInput)  {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your project: ',
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTableOfContents',
+            message: 'Would you like to add a Table of Contents?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'tableOfContents',
+            message: 'Provide a Table of Contents: ',
+            when: ({ confirmTableOfContents }) => confirmTableOfContents
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions: ',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage guidelines: ',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose the license your project will use.',
+            choices: [
+                'MIT',
+                'Apache-2.0',
+                'ISC',
+                'BSD-2-Clause',
+                'GPL-3.0-or-later',
+                'UNLICENSE',
+            ], 
+            default: 'MIT',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Provide instructions for contributing: ',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide instructions for running tests: ',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput)  {
+                    return true;
+                } else  {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput)  {
+                    return true;
+                } else  {
+                    console.log('Please enter your project GitHub link!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address as a contact for additional questions. '
+        },
+       
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+           if (err) {
+              console.log('Error. File was not created!');
+              reject(err);
+              return;
+            }
+            resolve({
+              ok: true,
+              message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
